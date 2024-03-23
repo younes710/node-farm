cf818b17f62172230f1d41f6deb577125496549c
Younes Abdi <realyoonesabdi@gmail.com>
2024-03-23 21:06:19 +0330

    Creating a simple web server

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // * fs module => file system
 const fs = require('fs');
+const http = require('http');
+
+////////////////////////////////////////
+// * FILES
 
 // * Blocking, synchronous way
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -7,11 +11,21 @@ const fs = require('fs');
 // fs.writeFileSync('./txt/output.txt', textOut);
 
 // * Non-blocking, asynchronous way
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
- fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-  fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
-   fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, (err) => {});
-  });
- });
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//   fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//    fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, (err) => {});
+//   });
+//  });
+// });
+// console.log('Will read file!');
+
+////////////////////////////////////////
+// * SERVER
+
+const server = http.createServer((req, res) => {
+ res.end('Hello from the server!');
 });
-console.log('Will read file!');
+server.listen(8000, '127.0.0.1', () => {
+ console.log('Listening to requests on port 8000');
+});
