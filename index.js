4df5897d390b68a4bb00a0d00d2098bde160e1d2
Younes Abdi <realyoonesabdi@gmail.com>
2024-03-23 19:53:55 +0330

    Reading and writing files

---
+++ index.js
@@ -0,0 +1,7 @@
+// * fs module => file system
+const fs = require('fs');
+
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+
+const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${new Date()}`;
+fs.writeFileSync('./txt/output.txt', textOut);
