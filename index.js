39ca35d1591d604c507e0eddcaa9c9a7db1433a6
Younes Abdi <realyoonesabdi@gmail.com>
2024-03-23 23:00:20 +0330

    Building a very simple API

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // * fs module => file system
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 ////////////////////////////////////////
 // * FILES
-
 // * Blocking, synchronous way
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 // const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${new Date()}`;
@@ -22,9 +22,28 @@ const http = require('http');
 
 ////////////////////////////////////////
 // * SERVER
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+// * convert string to object
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
- res.end('Hello from the server!');
+ const pathName = req.url;
+ if (pathName === '/' || pathName === '/overview') {
+  res.end('This is OVERVIEW!');
+ } else if (pathName === '/product') {
+  res.end('This is PRODUCT!');
+ } else if (pathName === '/api') {
+  res.writeHead(200, {
+   'Content-type': 'application/json',
+  });
+  res.end(data);
+ } else {
+  res.writeHead(404, {
+   'Content-type': 'text/html',
+   'my-own-header': 'hello-world',
+  });
+  res.end('<h1>Page not found!!!</h1>');
+ }
 });
 server.listen(8000, '127.0.0.1', () => {
  console.log('Listening to requests on port 8000');
