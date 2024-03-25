da735f75ca69543cd2db3cfb49fc78671b5c399a
Younes Abdi <realyoonesabdi@gmail.com>
2024-03-25 23:24:32 +0330

    Parsing variables from URLs

--- index.js
+++ index.js
@@ -54,10 +54,10 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
- const pathName = req.url;
+ const { query, pathname } = url.parse(req.url, true);
 
  // * Overview page
- if (pathName === '/' || pathName === '/overview') {
+ if (pathname === '/' || pathname === '/overview') {
   res.writeHead(200, {
    'Content-type': 'text/html',
   });
@@ -66,14 +66,16 @@ const server = http.createServer((req, res) => {
   res.end(output);
 
   // * Product page
- } else if (pathName === '/product') {
+ } else if (pathname === '/product') {
+  const product = dataObj[query.id];
+  const output = replaceTemplate(tempProduct, product);
   res.writeHead(200, {
    'Content-type': 'text/html',
   });
-  res.end(tempProduct);
+  res.end(output);
 
   // * API
- } else if (pathName === '/api') {
+ } else if (pathname === '/api') {
   res.writeHead(200, {
    'Content-type': 'application/json',
   });
